a6d5adf9a8185aff226d80deca1f9c202814a2eb
Luis Felipe S Botta <felipeex7@gmail.com>
2023-11-13 18:20:17 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -1,14 +1,19 @@
 
 
-const numero = 13;
+const numero = 11;
 
 const eNumeroPar = (numero % 2) === 0;
 
-if (eNumeroPar === true){
+if (numero === 0){
+    console.log("O número é inválido !");
+} else if (eNumeroPar === true){
     console.log("O numero é par !");
 } else {
     console.log("O numero é impar");
 }
+
+
+
 
 
 
