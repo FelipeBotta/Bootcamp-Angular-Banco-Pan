ba1579f1e4cc4e31c435e4cda1d5d71f181c36bd
Luis Felipe S Botta <felipeex7@gmail.com>
2023-11-19 18:01:14 -0300

    Create aula.js

---
+++ aula.js
@@ -0,0 +1,18 @@
+
+
+
+const pessoa = {
+    nome: "Luis Felipe",
+    idade: 24
+} ;
+
+
+pessoa.altura = 176;
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.altura);
+console.log(pessoa);
+
+
+
