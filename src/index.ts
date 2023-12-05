66c0d7862cc9303b69ee7b3b17804e4defd53b61
Luis Felipe S Botta <felipeex7@gmail.com>
2023-12-04 21:30:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-type heroi ={
-    name: string,
-    vulgo: string;
-}
+    
+    //tipos primitivos
+    let ligado: boolean = false;
+    let nome: string = "felipe";
+    let idade: number = 24;
+    let altura: number = 1.76;
+    
+    
+    //tipos especiais
+    let nulo: null = null;
+    let indefinido: undefined = undefined;
 
-
-
-function printaObjetos(pessoa: heroi){
-    console.log(pessoa);
-}
-
-printaObjetos({
-    name:"Bruce Wayne",
-    vulgo: "Batman"
-});
+    //tipos abrangentes
+    let retorno:void
+    let retornoView: any = 1;
