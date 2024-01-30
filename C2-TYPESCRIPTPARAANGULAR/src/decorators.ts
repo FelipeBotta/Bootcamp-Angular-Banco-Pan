46881628f5fcf8057b11b79c50b1d787bdf19ff8
Luis Felipe S Botta <felipeex7@gmail.com>
2024-01-30 15:47:54 -0300

    a

--- C2-TYPESCRIPTPARAANGULAR/src/decorators.ts
+++ C2-TYPESCRIPTPARAANGULAR/src/decorators.ts
@@ -9,3 +9,19 @@ class Funcionario{}
 
 @ExibirNome
 class Chefe{}
+
+
+//decorators (nivel de classe)
+
+function apiVersion(version: string){
+    return (target: any) => {
+        Object.assign(target.prototype, {__version: version, __name: "Felipe"})
+    }
+}
+
+@apiVersion("1.10")
+//class Api{}
+
+//const api = new Api();
+
+//console.log(api.__version)
--- C2-TYPESCRIPTPARAANGULAR/src/index.ts
+++ C2-TYPESCRIPTPARAANGULAR/src/index.ts
@@ -1,14 +1,31 @@
-//decorators
+//attribute decorator
 
-function apiVersion(version: string){
-    return (target: any) => {
-        Object.assign(target.prototype, {__version: version})
+function minLength(lenght: number){
+    return (target:any, key: string) => {
+        let _value = target[key];
+
+        const getter = () => _value;
+        
+        const setter = (value: string) =>{
+            if (value.length < lenght){
+                throw new Error(`Tamanho menor do que ${lenght}`);
+            }else{
+                _value = value;
+            };
+        };
+        
+        Object.defineProperty(target, key, {get: getter, set: setter})
+    };
+}
+
+class Api{
+    @minLength(3)
+    name:string
+
+    constructor(name:string){
+        this.name = name;
     }
 }
 
-@apiVersion("1.10")
-class Api{}
-
-const api = new Api();
-
-console.log(api.__version);
+const api = new Api("Produtos");
+console.log(api.name)
